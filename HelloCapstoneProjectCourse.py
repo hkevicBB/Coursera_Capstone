4acd9933704a86c11058fce32058ecb9c8a6c41b
Hrvoje Kevic <hkevic@blackberry.com>
2020-06-14 22:16:01 +0200

    Hello Capstone Project Course

---
+++ HelloCapstoneProjectCourse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Applied Data Science Capstone
+# 
+# ## Hello and welcome!
+# 
+# This notebook will be mainly used for the capstone project.
+# I hope you will enjoy the content and find it useful.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
